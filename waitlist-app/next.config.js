274205fa6cdc9a11fbbb6d19e5eaf8fb61590b78
Olaseni Bello <Olasenibello38@gmail.com>
2024-11-08 19:58:14 -0500

    config issues

--- waitlist-app/next.config.js
+++ waitlist-app/next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
+
+
